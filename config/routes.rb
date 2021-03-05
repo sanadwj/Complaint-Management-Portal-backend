ae54e6cd3b890c5d2350791545b927d66cde438b
Sanad <s_wj_88@hotmail.com>
2021-03-06 00:53:49 +0200

    Versioning the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  resources :sessions, only: [:create]
-  resources :registrations, only: [:create]
-  delete :logout, to: "sessions#logout"
-  get :logged_in, to: "sessions#logged_in"
+  namespace :api do
+    namespace :v1 do
+      resources :sessions, only: [:create]
+      resources :registrations, only: [:create]
+      resources :complaints, only: [:create]
+    end
+  end
 
-  resources :complaints, only: [:create]
+  delete :logout, to: "api/v1/sessions#logout"
+  get :logged_in, to: "api/v1/sessions#logged_in"
+
+
   root to: "static#index"
 end
